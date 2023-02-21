e5d2dca76c4fa4acf52e44715b08a2fc6dfe2ff9
Theo Marinho <thmarinho@outlook.fr>
2023-02-21 14:38:37 +0100

    add: make tab closeable

--- src/App.js
+++ src/App.js
@@ -7,26 +7,42 @@ import NotePad from './components/Tabs/NotePad';
 
 import './Reset.css'
 
-
 function App() {
   const { t } = useTranslation()
 
   const TABS = [
-    { name: t('fileFinder.tabs.home'), component: null, value: "profile" },
-    { name: t('fileFinder.tabs.formation'), component: null, value: "formation" },
-    { name: t('fileFinder.tabs.experiences'), component: null, value: "experiences" },
-    { name: t('fileFinder.tabs.skills'), component: null, value: "skills" },
-    { name: t('fileFinder.tabs.contact'), component: null, value: "contact" },
-    { name: t('fileFinder.tabs.notepad'), component: <NotePad />, value: "notepad"}
+    { name: t('fileFinder.tabs.home'), component: <div>profile</div>, value: "profile" },
+    { name: t('fileFinder.tabs.formation'), component: <div>formation</div>, value: "formation" },
+    { name: t('fileFinder.tabs.experiences'), component: <div>exp</div>, value: "experiences" },
+    { name: t('fileFinder.tabs.skills'), component: <div>skills</div>, value: "skills" },
+    { name: t('fileFinder.tabs.contact'), component: <div>contact</div>, value: "contact" },
+    { name: t('fileFinder.tabs.notepad'), component: <NotePad />, value: "notepad" },
   ]
 
-  const [selectedTab, setSelectedTab] = useState('notepad')
+  const [selectedTab, setSelectedTab] = useState(TABS[0].value)
+  const [closedTabs, setClosedTabs] = useState([])
+
+  const closeTab = (tabValue) => {
+    const _closedTabs = [...closedTabs, tabValue]
+    setClosedTabs(_closedTabs)
+
+    if (selectedTab === tabValue) {
+      const toOpenTab = TABS.find(tab => !_closedTabs.includes(tab.value))
+      console.log(toOpenTab)
+      setSelectedTab(toOpenTab?.value)
+    }
+  }
+
+  const openTab = (tabValue) => {
+    setClosedTabs(old => old.filter(tab => tab !== tabValue))
+    setSelectedTab(tabValue)
+  }
 
   return (
     <div className="h-screen w-screen flex">
       <NavBar />
-      <FileFinder tabs={TABS} selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-      <MainArea tabs={TABS} selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      <FileFinder tabs={TABS} selectedTab={selectedTab} setSelectedTab={openTab} />
+      <MainArea tabs={TABS} selectedTab={selectedTab} setSelectedTab={openTab} closeTab={closeTab} closedTabs={closedTabs} />
     </div>
   );
 }
--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,25 +1,30 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { useTranslation } from "react-i18next";
 
-const MainArea = ({ tabs, selectedTab, setSelectedTab }) => {
+const MainArea = ({ tabs, selectedTab, setSelectedTab, closeTab, closedTabs }) => {
   return (
     <div className="bg-main h-full flex-grow overflow-hidden">
       <div className="flex flex-col h-full">
         <div className="flex bg-fileFinder border-b-1 border-gray-900 overflow-x-auto">
-          {tabs.map(tab =>
-            <button
+          {tabs.filter(tab => !closedTabs.includes(tab.value)).map(tab =>
+            <div
               key={tab.value}
-              onClick={() => setSelectedTab(tab.value)}
-              className={'text-gray-500 flex items-center px-3 py-2 text-md group border-t-2 border-transparent '.concat(selectedTab === tab.value ? 'bg-main text-white' : 'hover:border-yellow-400')}
+              className={'flex text-gray-500 group border-t-2 border-transparent '.concat(selectedTab === tab.value ? 'bg-main text-white' : 'hover:border-yellow-400')}
             >
-              <img className="h-4 w-4 mr-1" src={`/icons/${tab.name.split('.').pop()}.svg`} alt="" />
-              {tab.name}
-              <XMarkIcon className={'h-3.5 w-3.5 ml-4 group-hover:text-white '.concat(selectedTab === tab.value ? 'text-white' : 'text-transparent')}/>
-            </button>
+              <button
+                onClick={() => setSelectedTab(tab.value)}
+                className="flex items-center pl-3 py-2 text-md"
+              >
+                <img className="h-4 w-4 mr-1" src={`/icons/${tab.name.split('.').pop()}.svg`} alt="" />
+                {tab.name}
+              </button>
+              <button onClick={() => closeTab(tab.value)}>
+                <XMarkIcon className="h-3.5 w-3.5 mx-3 group-hover:text-white" />
+              </button>
+            </div>
           )}
         </div>
         <div className={'flex-grow h-full '.concat(selectedTab === 'notepad' ? '' : 'p-4')}>
-          {tabs.find(tab => tab.value === selectedTab ).component}
+          {tabs.find(tab => tab.value === selectedTab)?.component || <div>default</div>}
         </div>
       </div>
     </div>
